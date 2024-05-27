76e79d2122c6585d0761d025d5309b36e84abe51
DollyTheSau <54324611+DollyTheSau@users.noreply.github.com>
2024-05-27 18:08:47 +0200

    Initial comments for the boys exam prep

--- src/main/java/app.java
+++ src/main/java/app.java
@@ -12,25 +12,28 @@ public class app {
     public static void main(String[] args) throws IOException {
     try {
         
+        //Gains written code from program file.
         String inputFile = "src/main/java/program.txt";
         String inputText = readFile(inputFile);
-        
         CharStream input = CharStreams.fromString(inputText);
 
+        //Lexes written input
         MusicLanguageLexer lexer = new MusicLanguageLexer(input);
         lexer.removeErrorListeners();
         lexer.addErrorListener(ErrorChecker.INSTANCE);
         
+        //Tokenizes Lexed input
         CommonTokenStream tokens = new CommonTokenStream(lexer);
         
+        //Parses tokens
         MusicLanguageParser parser = new MusicLanguageParser(tokens);
         
         parser.removeErrorListeners();
         parser.addErrorListener(ErrorChecker.INSTANCE);
-        ParseTree tree = parser.program();
-        Visitor astBuilder = new Visitor();
 
-        
+        //Runs through entire code
+        ParseTree tree = parser.program();
+        Visitor astBuilder = new Visitor();        
         ASTNode ast = astBuilder.visit(tree);
       
         } catch (Exception e) {
@@ -38,7 +41,7 @@ public class app {
         }
     };
 
-    
+    //Creates a string from files
     private static String readFile(String fileName) throws IOException {
         StringBuilder content = new StringBuilder();
         try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
